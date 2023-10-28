2da937944f2712379ee08557495858adeea55e00
Miglena Medintsova <142172186+Miglena-Medintsova@users.noreply.github.com>
2023-10-28 19:24:11 +0300

    Add files via upload

---
+++ petShop.js
@@ -0,0 +1,7 @@
+function petShop (input) {
+    let dogs = Number(input.shift());
+    let other = Number(input.shift());
+    let price = dogs * 2.5 + other * 4;
+    console.log(price.toFixed(2) + " lv.");
+  }
+  petShop(['13', '9'])
