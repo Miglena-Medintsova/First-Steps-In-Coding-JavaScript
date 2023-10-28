3c0b85823fdae0f6100b3d2e57b21adf5faabd12
Miglena Medintsova <142172186+Miglena-Medintsova@users.noreply.github.com>
2023-10-28 19:22:38 +0300

    Add files via upload

---
+++ projectsCreation.js
@@ -0,0 +1,7 @@
+function projectCreation(input) {
+    let name = input[0];
+    let numberOfProjects = Number(input[1]);
+    let hours = numberOfProjects * 3;
+    console.log(`The architect ${name} will need ${hours} hours to complete ${numberOfProjects} project/s.`);
+}
+projectCreation(['Sanya', '9']);
