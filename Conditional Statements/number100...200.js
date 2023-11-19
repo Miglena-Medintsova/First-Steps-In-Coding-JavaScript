e24d74250a1cae7fab8806bbd3dfd9613da585c1
Miglena Medintsova <142172186+Miglena-Medintsova@users.noreply.github.com>
2023-11-19 17:16:20 +0200

    Add files via upload

---
+++ Conditional Statements/number100...200.js
@@ -0,0 +1,16 @@
+function number100200(input) {
+    let num1 = Number(input[0]);
+    let min = 100;
+    let max = 200;
+    if (num1 < min) {
+        console.log("Less than 100");
+    } else if (num1 >= min && num1 <= max) {
+        console.log("Between 100 and 200");
+    } else if (num1 > max) {
+        console.log("Greater than 200");
+    }
+
+}
+number100200(["210"])
+
+
