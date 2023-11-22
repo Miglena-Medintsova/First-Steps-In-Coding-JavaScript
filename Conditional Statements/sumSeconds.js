5e21db0a3a704b9d6d6d40a4c9310e5e8ac2cec9
Miglena Medintsova <142172186+Miglena-Medintsova@users.noreply.github.com>
2023-11-22 11:32:40 +0200

    Add files via upload

---
+++ Conditional Statements/sumSeconds.js
@@ -0,0 +1,19 @@
+function sumSeconds(input){
+let first = Number(input[0]);
+let second = Number(input[1]);
+let third = Number(input[2]);
+let totalTime = first + second + third;
+let minutesSum = Math.floor(totalTime / 60);
+let secondsSum = totalTime % 60;
+if(secondsSum < 10){
+    console.log(`${minutesSum}:0${secondsSum}`);
+} else {
+    console.log(`${minutesSum}:${secondsSum}`);
+}
+
+}
+sumSeconds(["14", "12",
+"10"])
+
+
+
