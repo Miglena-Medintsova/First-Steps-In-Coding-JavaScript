72efbaea69ecefd76ef369c9589fbb6563c1a68f
Miglena Medintsova <142172186+Miglena-Medintsova@users.noreply.github.com>
2023-12-22 13:15:49 +0200

    Add files via upload

---
+++ Conditional Statements Advanced/invalidNumber.js
@@ -0,0 +1,9 @@
+function invalidNumber(input) {
+    let numb = Number(input[0]);
+    let isValid = (numb === 0 || numb >= 100 && numb <= 200);
+    if (!isValid) {
+        console.log("invalid");
+    }
+}
+
+invalidNumber(["150"])
