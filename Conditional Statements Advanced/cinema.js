44a451831938954e5787cc253b52761c11d446a7
Miglena Medintsova <142172186+Miglena-Medintsova@users.noreply.github.com>
2023-12-25 18:07:03 +0200

    Add files via upload

---
+++ Conditional Statements Advanced/cinema.js
@@ -0,0 +1,25 @@
+function cinema(input) {
+let movieType = input[0];
+let r = Number(input[1]);
+let c = Number(input[2]);
+
+let totalPrice = 0;
+
+if(movieType === 'Premiere') {
+    totalPrice = r * c * 12;
+
+} else if(movieType === 'Normal') {
+    totalPrice = r * c * 7.5;
+
+} else if(movieType === 'Discount') {
+    totalPrice = r * c * 5;
+
+}
+console.log(`${totalPrice.toFixed(2)} leva`);
+}
+cinema(["Discount",
+"12",
+"30"])
+
+
+
