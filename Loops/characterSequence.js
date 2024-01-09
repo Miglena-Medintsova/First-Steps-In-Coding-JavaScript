086de5b2dc2bbe5cbd90a0c9bad8abb07314474c
Miglena Medintsova <142172186+Miglena-Medintsova@users.noreply.github.com>
2024-01-09 22:44:18 +0200

    Add files via upload

---
+++ Loops/characterSequence.js
@@ -0,0 +1,8 @@
+function characterSequence (input) {
+let name = input[0];
+for(let a = 0; a < name.length; a++) {
+    let letter = name[a];
+    console.log(letter);
+}
+}
+characterSequence(["softuni"])
