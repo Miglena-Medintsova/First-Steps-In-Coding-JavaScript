1e1545f5c9da0b46d88beb345a8b33898f83d5f2
Miglena Medintsova <142172186+Miglena-Medintsova@users.noreply.github.com>
2024-01-11 16:59:03 +0200

    Add files via upload

---
+++ Loops/sumOfNumbers.js
@@ -0,0 +1,15 @@
+function sumOfNumbers(input) {
+    
+    let numberAsString = input.toString();
+    let sum = 0;
+
+    for (let index = 0; index < numberAsString.length; index++) {
+        let currentNumber = Number(numberAsString[index]);
+
+        sum = sum + currentNumber;
+    }
+console.log(`The sum of the digits is:${sum}`);
+
+}
+
+sumOfNumbers(["1234"])
