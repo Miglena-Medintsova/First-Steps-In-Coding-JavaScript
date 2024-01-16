80a6b72cf7be1dad7e7f4b677e96c8b1f61a596b
Miglena Medintsova <142172186+Miglena-Medintsova@users.noreply.github.com>
2024-01-16 17:23:10 +0200

    Add files via upload

---
+++ Loops/cleverLily.js
@@ -0,0 +1,27 @@
+function cleverLily(input) {
+let age = Number(input[0]);
+let washerPrice = Number(input[1]);
+let toyPrice = Number(input[2]);
+
+let moneySaved = 0;
+let moneyGiven = 10;
+
+for (let i = 1; i <= age; i++) {
+    if (i % 2 !== 0) {
+        moneySaved += toyPrice;
+    } else {
+        moneySaved += (moneyGiven - 1);
+        moneyGiven += 10;
+    }
+}
+ if (moneySaved >= washerPrice) {
+    let moneyLeft = moneySaved - washerPrice;
+    console.log(`Yes! ${moneyLeft.toFixed(2)}`);
+ } else {
+    let moneyNeeded = washerPrice - moneySaved;
+    console.log(`No! ${moneyNeeded.toFixed(2)}`);
+ }
+
+
+}
+cleverLily(["10","170.00","6"])
